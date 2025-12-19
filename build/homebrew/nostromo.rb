13e7298ef98734c5b80ba493e08c5d7e15590711
Guillermo Verdu Alcaraz <verdug@basfad.basf.net>
2025-12-19 13:34:43 +0100

    Update Homebrew formula to use PyPI as source
    - No GitHub authentication required
    - Use actual SHA256 hashes from PyPI

--- build/homebrew/nostromo.rb
+++ build/homebrew/nostromo.rb
@@ -16,23 +16,15 @@ class Nostromo < Formula
 
   desc "MU-TH-UR 6000 AI Interface - An Aliens-themed CLI chatbot"
   homepage "https://github.com/especialista-seta/nostromo"
-  url "https://github.com/especialista-seta/nostromo/releases/download/v0.1.0/nostromo-0.1.0.tar.gz"
-  sha256 "TODO_UPDATE_SHA256_ON_RELEASE"
+  url "https://files.pythonhosted.org/packages/source/n/nostromo-cli/nostromo_cli-0.1.0.tar.gz"
+  sha256 "9fbbfc6c7a84c750bf638067b805c8d4b9d824083ae2d6fec132c63a097c6c3a"
   license "MIT"
-
-  # Bottle block will be added by Homebrew CI
-  # bottle do
-  #   sha256 cellar: :any_skip_relocation, arm64_sonoma: "..."
-  #   sha256 cellar: :any_skip_relocation, ventura: "..."
-  #   sha256 cellar: :any_skip_relocation, x86_64_linux: "..."
-  # end
 
   depends_on "python@3.11"
 
-  # Main package
-  resource "nostromo-cli" do
-    url "https://files.pythonhosted.org/packages/.../nostromo_cli-0.1.0.tar.gz"
-    sha256 "TODO_UPDATE_SHA256"
+  resource "nostromo-core" do
+    url "https://files.pythonhosted.org/packages/source/n/nostromo-core/nostromo_core-0.1.0.tar.gz"
+    sha256 "0b7c1958cfa5bcfb469adf4edd76eb0aa6859d9dd9633cb6de76502e2e503d2a"
   end
 
   # Dependencies will be auto-populated by brew audit
